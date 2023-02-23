184cb4366f797d97859c66845174ef58b3a546db
Ana Skarica <ana.skarica.as@gmail.com>
2023-02-23 21:37:51 +0100

    add design

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-"use strict";
 
 const btnAdd = document.getElementById("btn-add");
 const btnRemoveAll = document.getElementById("btn-remove-all");
 
 btnAdd.addEventListener("click", () => {
-    const listItem = document.createElement("li");
-    let inputText = document.getElementById("input").value;
-    let inputNode = document.createTextNode(inputText);
-    listItem.appendChild(inputNode);
+    const item = document.createElement("div");
+    let inputNode = document.createTextNode(document.getElementById("input").value);
+    item.appendChild(inputNode);
+    item.classList.add("item")
     const items = document.getElementById("items-container");
-    items.appendChild(listItem);
+    items.appendChild(item);
+    document.getElementById("input").value = "";
 })
 
 btnRemoveAll.addEventListener("click", () => {
-    const items = document.getElementById("items-container").innerHTML = "";
+    document.getElementById("items-container").innerHTML = "";
 })
