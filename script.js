32b16a87e9ca707019fedae3e18968d9683da329
Ana Skarica <ana.skarica.as@gmail.com>
2023-02-23 19:51:29 +0100

    add remove all

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const btnAdd = document.getElementById("btn-add");
-const btnRemoveAll = document.getElementById("btn-add");
+const btnRemoveAll = document.getElementById("btn-remove-all");
 
 btnAdd.addEventListener("click", () => {
     const listItem = document.createElement("li");
@@ -10,4 +10,8 @@ btnAdd.addEventListener("click", () => {
     listItem.appendChild(inputNode);
     const items = document.getElementById("items-container");
     items.appendChild(listItem);
+})
+
+btnRemoveAll.addEventListener("click", () => {
+    const items = document.getElementById("items-container").innerHTML = "";
 })
